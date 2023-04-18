836db6d18339ff5c30769ed619c02a7e00a07efd
Nicholus Munene Gitonga <35835432+nicholusmunene@users.noreply.github.com>
2023-04-18 10:56:16 +0300

    Add files via upload

---
+++ register.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Signup Form</title>
+</head>
+<body>
+	<h2>Signup Form</h2>
+	<form method="POST" action="signup.php">
+		<label for="usrname">Username:</label>
+
+		<input type="text" name="username" id="username"><br><br>
+		<label for="email">Email:</label>
+
+		<input type="email" name="email" id="email"><br><br>
+		<label for="password">Password:</label>
+
+		<input type="password" name="password" id="password"><br><br>
+		<input type="submit" name="submit" value="Sign up">
+		
+	</form>
+</body>
+</html>
+
